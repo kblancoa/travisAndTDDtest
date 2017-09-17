8d85eda34c326960830f01226a5c57f3bcc0b9f6
kblancoa <kiko.18.05.4@gmail.com>
2017-09-17 19:43:15 +0200

    Testing travis fail

--- change.js
+++ change.js
@@ -13,7 +13,7 @@ function getChange (totalPayable, cashPaid) {
     var coins = [200, 100, 50, 20, 10, 5, 2, 1];
 
     if(totalPayable === cashPaid){
-        return [1];
+        return null;
     }
 
     var change = cashPaid -totalPayable ;
