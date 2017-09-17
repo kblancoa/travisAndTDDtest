a01c902b046d71805f9d8a8475b91e030f64fccd
kblancoa <kiko.18.05.4@gmail.com>
2017-09-17 19:19:56 +0200

    Commit with travis

---
+++ change.js
@@ -0,0 +1,40 @@
+
+/* The code block below ONLY Applies to Node.js - This Demonstrates
+   re-useability of JS code in both Back-end and Front-end! #isomorphic */
+/* istanbul ignore next */
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = getChange;  // allows CommonJS/Node.js require()
+}
+
+function getChange (totalPayable, cashPaid) {
+
+    var returnCoins=[];
+
+    var coins = [200, 100, 50, 20, 10, 5, 2, 1];
+
+    if(totalPayable === cashPaid){
+        return returnCoins;
+    }
+
+    var change = cashPaid -totalPayable ;
+
+    while (change > 0) {
+        var currentCoin =coins.shift();
+
+        if(currentCoin <= change){
+
+            var countCurrentCoin = Math.floor(change / currentCoin);
+
+            do{
+                returnCoins.push(currentCoin);
+                countCurrentCoin--;
+            }while(countCurrentCoin> 0);
+            change = change % currentCoin;
+        }
+    }
+
+
+    return returnCoins;
+
+
+}
